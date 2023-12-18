dd07dab0eb262ac7d99244df37123a448c1bd096
田中里歩 <tanaka.riho24@gmail.com>
2023-12-18 14:27:27 +0900

    🩹Add:プロダクション環境のDATABESE_URL追加

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -3,10 +3,4 @@ set -e
 
 rm -f /app/tmp/pids/server.pid
 
-bundle install
-bundle exec rails db:migrate  
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
-
 exec "$@"
