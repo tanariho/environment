51edd5852b8821a59ba791ef613abbaa12fc8908
田中里歩 <tanaka.riho24@gmail.com>
2023-12-18 12:56:52 +0900

    ホストの追加

--- back/config/environments/production.rb
+++ back/config/environments/production.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts << "https://environment-ud2f.onrender.com"
+  config.hosts << "environment-ud2f.onrender.com"
 end
