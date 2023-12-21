6273b959d84fbeea1d400a8eebca6c50abf8ffe0
田中里歩 <tanaka.riho24@gmail.com>
2023-12-21 12:02:39 +0900

    ✨Add:モデルのバリデーションを追加

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :provider, presence: true
+  validates :uid, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 256 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 256 }
 end
