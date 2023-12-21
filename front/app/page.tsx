6a03e6099d2623bfa7197d6a5b23660dbf0835ce
田中里歩 <tanaka.riho24@gmail.com>
2023-12-21 10:48:49 +0900

    ✨Add:NextAuth,Google認証でログイン

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,74 +1,25 @@
-"use client";
-import React, { useEffect, useState } from "react";
-
-type Post = {
-  id: number;
-  title: string;
-};
+import React from "react";
+import Title from "./components/top/Title"
+import Footer from "./components/top/Footer";
+import Header from "./components/top/Header";
+import { useSession } from 'next-auth/react'
+import Loginbtn from "./components/Login-btn";
 
 export default function Home() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [newTitle, setNewTitle] = useState("");
-
-  const fetchPosts = async () => {
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
-      if (!response.ok) {
-        throw new Error("データの取得に失敗しました");
-      }
-      const data = await response.json();
-      setPosts(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ title: newTitle }),
-      });
-      if (!response.ok) {
-        throw new Error("投稿に失敗しましたよん");
-      }
-      setNewTitle("");
-      fetchPosts();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h2 className="text-3xl mb-4">記事の一覧</h2>
-      <form onSubmit={handleSubmit} className="mt-4 mb-4">
-        <input
-          type="text"
-          value={newTitle}
-          onChange={(e) => setNewTitle(e.target.value)}
-          placeholder="新しい投稿のタイトル"
-          className="mr-2 p-2 border"
-        />
-        <button type="submit" className="p-2 bg-blue-500 text-white">
-          投稿する
-        </button>
-      </form>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-          
-        ))}
-      </ul>
-    </main>
+    <div>
+      <main className="flex bg-gradient-to-r from-lime-200 to-lime-50 relative">
+
+        <section className="flex-grow min-h-screen">
+          <Header />
+          <Title />
+      <div>
+        <h1>next-rails-google-auth</h1>
+          <Loginbtn />
+      </div>
+          <Footer />
+        </section>
+      </main>
+    </div>
   );
 }
-console.log(process.env.NEXT_PUBLIC_API_URL);
